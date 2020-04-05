51ff646d2a735e3df899a5af271bf502fb0760bb
LunatiX <56197620+Lunatix01@users.noreply.github.com>
2020-04-05 21:21:10 +0300

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 getIp=$(ifconfig | grep "192" | sed -e 's/^[[:space:]]*//'| cut -d " " -f 2 |  awk -F'/' '{print $1}')
-showIp=$(ping $getIp -c 1 | grep "packet"| cut -d "," -f 3|  awk -F 'packet' '{print $1}')
+packetLoss=$(ping $getIp -c 1 | grep "packet"| cut -d "," -f 3|  awk -F 'packet' '{print $1}')
 time=$(ping $getIp -c 1 | grep "time"| cut -d " " -f 7| tr -d [:alpha:])
 echo "your local ip: "$getIp
-echo "Packet loss:"$showIp
+echo "Packet loss:"$packetLoss
 echo "time= "$time
-if [ showIp==100% ]
+if [ packetLoss==100% ]
 then
 echo "your ip is down"
 else echo "your ip is up"
